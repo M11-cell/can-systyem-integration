20c17bd4b0e5ee8e638af47f4cc6cdbfe94047ff
M11-cell <michael.lythgoe@spaceconcordia.ca>
2026-03-15 23:51:59 -0400

    added device_id, manufacturer, and deviceType enums

--- src/can-integration/include/framer.hpp
+++ src/can-integration/include/framer.hpp
@@ -4,50 +4,69 @@
 #include "can_interface.hpp"
 
 //6 bites
-struct Device_Id{   
+namespace DeviceId{
+    enum : uint8_t{   
 
-    uint32_t BAB:6 = 0x00; 
-    uint32_t JETSON:6 = 0x01;
-    uint32_t AIRLINK_RECEIVER:6 = 0x02;
-    uint32_t WHEEL_HARD_STOP:6 = 0x03; 
-    uint32_t ARM_HARD_STOP:6 = 0X04; 
-    uint32_t JMSB:6 = 0x05; 
-    uint32_t COMPAT_BOARD:6 = 0x06;
-    uint32_t BASE_ENCODER:6 = 0x07;
-    uint32_t SHOULDER_ENCODER:6 = 0x08;
-    uint32_t ELBOWsrc_ENCODER:6 = 0x09;
-    uint32_t FOREARM_ENCODER:6 = 0x0A;
-    uint32_t WRIST_ENCODER:6 = 0x0B;
-    uint32_t SPIN_SERVO_ENCODER:6 = 0x0C;
-    uint32_t CLAMP_SERVO_ENCODER:6 = 0x0D; 
-    uint32_t HUB:6 = 0x0E; 
-    uint32_t RESERVED_PAYLOAD:6 = 0x0F; 
-    uint32_t SIL:6 = 0x10; 
-}; 
+        BAB = 0x00,
+        JETSON = 0x01,
+        AIRLINK_RECEIVER = 0x02,
+        WHEEL_HARD_STOP = 0x03,
+        ARM_HARD_STOP = 0X04,
+        JMSB = 0x05, 
+        COMPAT_BOARD = 0x06,
+        BASE_ENCODER = 0x07,
+        SHOULDER_ENCODER = 0x08,
+        ELBOW_ENCODER = 0x09,
+        FOREARM_ENCODER = 0x0A,
+        WRIST_ENCODER = 0x0B,
+        SPIN_SERVO_ENCODER = 0x0C,
+        CLAMP_SERVO_ENCODER = 0x0D, 
+        HUB = 0x0E,
+        RESERVED_PAYLOAD = 0x0F, 
+        SIL = 0x10
+    }; 
+}
 
 // 8 bits
-struct manufacturer{
-    // 8 bit message 
-    uint32_t TEAM_USE:8 = 0x08; 
-    // 5 bit message
-    uint32_t REV_ROBOTICS:5 = 0x05; 
-
-};
+namespace manufacturer{
+    enum : uint8_t{ 
+        TEAM_USE = 0x08, 
+        REV_ROBOTICS = 0x05,
+    };
+}
 
 // 10 bits
-struct instruction{
-
-
-    
-};
+namespace instructions{
+    enum : uint16_t{
+        CUT_POWER = 0x00,
+        SEND_VELOCITY = 0,
+        SEND_POSITION = 0, 
+        STATUS = 0, 
+        CHECK_VOLTAGE = 0, 
+        TEMP = 0, 
+        STOP = 0, 
+        CHECK_HEALTH = 0, 
+        ZERO_ENCODERS = 0, 
+        SET_LED_COLOR = 0,
+    };
+}
 
 //5 bits
-struct devicetype{
-    uint32_t MOTOR_CONTROLLER:5 = 0x2;
-    uint32_t ENCODER:5 = 0x07;
-    uint32_t COLOR_SENS0R:5 = 0x0D; 
-    uint32_t SERVO_CONTROLLER:5 = 0x0C;
-    uint32_t RESERVED:5 = 0x0E; 
+namespace deviceType{
+    enum : uint8_t{
+        MOTOR_CONTROLLER = 0x2,
+        ENCODER = 0x07,
+        COLOR_SENS0R = 0x0D, 
+        SERVO_CONTROLLER = 0x0C,
+        RESERVED = 0x0E
+    };
+}
+
+struct fields{
+    uint32_t device_id: 6;
+    uint32_t device_type: 5; 
+    uint32_t instruction: 10; 
+    uint32_t manufacturer: 8; 
 };
 
 class Framer{
